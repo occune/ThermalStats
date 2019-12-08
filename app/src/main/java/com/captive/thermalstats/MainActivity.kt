094f8b416a478bed72c1a25eda9bd2b3be1d91b6
occune <batyamedic@gmail.com>
2019-12-08 13:47:10 +0300

    test powermanager commit

--- app/src/main/java/com/captive/thermalstats/MainActivity.kt
+++ app/src/main/java/com/captive/thermalstats/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.captive.thermalstats
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PowerManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        val current_status = powerManager.currentThermalStatus
+        thermal_id.setText(current_status)
     }
 }
